2fe162602d0b97d296c687ca18afb38a1dfc97b2
seunggyun-jeong <jskyun98@gmail.com>
2025-01-03 00:38:14 +0900

    [Add] 폴더 구조 초안

---
+++ Staccato-iOS/Staccato-iOS/Presentation/View/SignIn/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  Staccato-iOS
+//
+//  Created by Gyunni on 1/3/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
---
+++ Staccato-iOS/Staccato-iOS/Presentation/ViewModel/SignIn/SignInViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignInViewModel.swift
+//  Staccato-iOS
+//
+//  Created by Gyunni on 1/3/25.
+//
+
+import Foundation
+
+@Observable
+class SignInViewModel {
+
+}

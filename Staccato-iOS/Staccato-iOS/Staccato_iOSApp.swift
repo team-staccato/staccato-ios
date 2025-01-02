58c4793e78fa8a73c9d230bad3fe471a9614b9ae
seunggyun-jeong <jskyun98@gmail.com>
2025-01-02 22:36:50 +0900

    [Init] Project Init

---
+++ Staccato-iOS/Staccato-iOS/Staccato_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Staccato_iOSApp.swift
+//  Staccato-iOS
+//
+//  Created by Gyunni on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Staccato_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

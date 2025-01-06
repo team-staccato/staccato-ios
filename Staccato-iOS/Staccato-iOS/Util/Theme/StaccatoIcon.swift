c378482fb040254f011eac9457bafb6b18d33fc0
seunggyun-jeong <jskyun98@gmail.com>
2025-01-06 23:56:35 +0900

    [Refactor] 디자인시스템 관리 폴더 생성

---
+++ Staccato-iOS/Staccato-iOS/Util/Theme/StaccatoIcon.swift
@@ -0,0 +1,43 @@
+//
+//  StaccatoIcon.swift
+//  Staccato-iOS
+//
+//  Created by Gyunni on 1/3/25.
+//
+
+/// `StaccatoIcon`은 앱 전반에서 사용하는 **아이콘(SF Symbols 혹은 커스텀 에셋)**을
+/// 열거형으로 관리하기 위한 타입입니다.
+///
+/// - Usage:
+///   ```swift
+///   // SF Symbol 사용 예시
+///   Image(systemName: StaccatoIcon.chevronLeft.rawValue)
+///     .resizable()
+///     .frame(width: 24, height: 24)
+///   ```
+enum StaccatoIcon: String {
+    // MARK: - Toolbar
+    case chevronLeft = "chevron.left"
+
+    // MARK: - Categories
+    case folderFill = "folder.fill"
+    case sliderHorizontal3 = "slider.horizontal.3"
+
+    // MARK: - Category
+    case pencilLine = "pencil.line"
+    case calendar = "calendar"
+
+    // MARK: - Category Creation/Update
+    case camera = "camera"
+    case xCircleFill = "x.circle.fill"
+
+    // MARK: - Staccato Creation/Update
+    case location = "location"
+    case minusCircle = "minus.circle"
+
+    // MARK: - MyPage
+    case pencilCircleFill = "pencil.circle.fill"
+    case squareOnSquare = "square.on.square"
+    case chevronRight = "chevron.right"
+    case personCircleFill = "person.circle.fill"
+}

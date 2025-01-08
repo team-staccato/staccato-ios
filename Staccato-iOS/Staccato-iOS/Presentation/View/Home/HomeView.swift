b1726d0b33412564fe139e8c187d3bfd1181d567
Yurim Kim <yurim830@gmail.com>
2025-01-09 02:44:57 +0900

    [Add] #4 - HomeView 생성

---
+++ Staccato-iOS/Staccato-iOS/Presentation/View/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Staccato-iOS
+//
+//  Created by 김유림 on 1/9/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}

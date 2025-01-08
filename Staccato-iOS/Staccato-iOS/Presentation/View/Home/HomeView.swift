5c10940e1225c46df0b79fe92effd76d8c033798
Yurim Kim <yurim830@gmail.com>
2025-01-09 05:54:37 +0900

    [Feat] #4 - 지도 및 마이페이지 버튼 추가

--- Staccato-iOS/Staccato-iOS/Presentation/View/Home/HomeView.swift
+++ Staccato-iOS/Staccato-iOS/Presentation/View/Home/HomeView.swift
@@ -5,11 +5,42 @@
 //  Created by 김유림 on 1/9/25.
 //
 
+import GoogleMaps
+
 import SwiftUI
 
 struct HomeView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack(alignment: .topLeading) {
+                MapViewControllerBridge()
+                    .edgesIgnoringSafeArea(.all)
+                
+                NavigationLink(destination: TempMyPageView()) {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40, height: 40)
+                        .background(Color.white)
+                        .foregroundStyle(.gray3)
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle().stroke(Color.white, lineWidth: 2)
+                        }
+                }
+                .padding(20)
+            }
+        }
+    }
+}
+
+struct TempMyPageView: View {
+    var body: some View {
+        Text("My Page")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding()
     }
 }
 

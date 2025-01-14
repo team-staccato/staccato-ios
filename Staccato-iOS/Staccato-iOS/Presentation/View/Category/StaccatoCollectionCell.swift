df07d06bc726b0891d3239022984031556712433
seunggyun-jeong <jskyun98@gmail.com>
2025-01-14 16:57:00 +0800

    [Feat] 연월일 String으로 표시하는 Extension 추가

---
+++ Staccato-iOS/Staccato-iOS/Presentation/View/Category/StaccatoCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  StaccatoCollectionCell.swift
+//  Staccato-iOS
+//
+//  Created by Gyunni on 1/14/25.
+//
+
+import SwiftUI
+
+struct StaccatoCollectionCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StaccatoCollectionCell()
+}

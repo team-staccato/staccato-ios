12257dfb5cf49d4b78a77e701a6be00d558868c7
seunggyun-jeong <jskyun98@gmail.com>
2025-01-14 16:57:18 +0800

    [Feat] 스타카토 Collection Cell 구현

--- Staccato-iOS/Staccato-iOS/Presentation/View/Category/StaccatoCollectionCell.swift
+++ Staccato-iOS/Staccato-iOS/Presentation/View/Category/StaccatoCollectionCell.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct StaccatoCollectionCell: View {
+    let title: String
+    let date: Date
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomLeading) {
+            // TODO: 이미지로 변경
+            Rectangle()
+                .foregroundStyle(.red)
+
+            linearGradient
+
+            VStack(alignment: .leading, spacing: 7) {
+                Text(title)
+                    .typography(.title3)
+
+                HStack(spacing: 4) {
+                    Image(.calendar)
+
+                    Text(date.formattedAsFullDate)
+                }
+                .typography(.body4)
+            }
+            .padding(.bottom, 10)
+            .padding(.leading, 12)
+            .foregroundStyle(.white)
+        }
+        .frame(width: 160, height: 200)
+        .clipShape(RoundedRectangle(cornerRadius: 8))
     }
 }
 
 #Preview {
-    StaccatoCollectionCell()
+    StaccatoCollectionCell(title: "광안리", date: .now)
+}
+
+extension StaccatoCollectionCell {
+    private var linearGradient: LinearGradient {
+        LinearGradient(
+            gradient: Gradient(stops: [
+                .init(color: Color.black.opacity(0), location: 0.0),
+                .init(color: Color.black.opacity(0.5), location: 0.65),
+                .init(color: Color.black.opacity(0.7), location: 1.0)
+            ]),
+            startPoint: .top,
+            endPoint: .bottom
+        )
+    }
 }

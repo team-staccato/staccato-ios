1f9ca68d881992c3587334e7229993611e15120b
kang-jae-heok <kjhabc8707@naver.com>
2025-01-21 00:51:00 +1100

    [Feat]: SignIn View UI 제작(이미지 빼고)

--- Staccato-iOS/Staccato-iOS/Presentation/View/SignIn/SignInView.swift
+++ Staccato-iOS/Staccato-iOS/Presentation/View/SignIn/SignInView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct SignInView: View {
+    
+    @State private var nickName: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            //TODO: 이미지 바꿔야됨
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            TextField("닉네임을 입력해주세요", text: $nickName)
+                .padding()
+                .background(.gray1)
+                .cornerRadius(4)
+                .shadow(color: .gray3.opacity(0.5), radius: 10, x: 0, y: 5)
+                .onChange(of: nickName) { _, newValue in
+                    if newValue.count > 20 {
+                        nickName = String(newValue.prefix(20))
+                    }
+                }
+            Text("\(nickName.count)/20")
+                .typography(.body4)
+                .foregroundStyle(.gray3)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.bottom)
+            Button(action: {
+                print("시작하기 버튼이 눌렸습니다.")
+            }) {
+                Text("시작하기")
+                    .typography(.title3)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 45)
+                    .background(.gray1)
+                    .foregroundColor(.gray4)
+                    .cornerRadius(4)
+            }
+            .padding(.vertical)
+            Button(action: {
+                print("시작하기 버튼이 눌렸습니다.")
+            }) {
+                Text("이전 기록을 불러오려면 여기를 눌러주세요")
+                    .typography(.body4)
+                    .foregroundColor(.gray4)
+                    .underline()
+            }
+            .padding(.vertical)
+        }
+        .padding(.horizontal, 24)
     }
 }
 

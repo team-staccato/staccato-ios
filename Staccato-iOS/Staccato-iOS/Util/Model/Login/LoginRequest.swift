f28219e5c08101a962838a1ccc6c907d84b77796
kang-jae-heok <kjhabc8707@naver.com>
2025-02-01 01:27:25 +1100

    [Feat] Network Layer 추가

---
+++ Staccato-iOS/Staccato-iOS/Util/Model/Login/LoginRequest.swift
@@ -0,0 +1,12 @@
+//
+//  LoginRequest.swift
+//  Staccato-iOS
+//
+//  Created by 강재혁 on 1/9/25.
+//
+
+import Foundation
+
+struct LoginRequest: Codable {
+    let nickname: String            // 닉네임
+}

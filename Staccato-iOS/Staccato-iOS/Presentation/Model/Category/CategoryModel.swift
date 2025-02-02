4d1a4ad583671767aeea106cfb6ea443290a2a82
Yurim Kim <yurim830@gmail.com>
2025-02-02 18:16:17 +0900

    [Add] #17 - CategoryModel 생성

---
+++ Staccato-iOS/Staccato-iOS/Presentation/Model/Category/CategoryModel.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryModel.swift
+//  Staccato-iOS
+//
+//  Created by 김유림 on 2/2/25.
+//
+
+import SwiftUI
+
+struct CategoryModel: Equatable {
+    
+    let id: Int
+    
+    let thumbNailUrl: Image
+    
+    let title: String
+    
+    let startAt: String
+    
+    let endAt: String
+    
+}

eaad7381e99c03d07816c789304110c38fd09df0
Yurim Kim <yurim830@gmail.com>
2025-02-02 17:29:56 +0900

    [Add] #17 - CategoryListView(홈 모달 뷰) 생성

---
+++ Staccato-iOS/Staccato-iOS/Presentation/View/CategoryList/CategoryListView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryListView.swift
+//  Staccato-iOS
+//
+//  Created by 김유림 on 2/2/25.
+//
+
+import SwiftUI
+
+struct CategoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryListView()
+}

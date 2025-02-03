063d906fbf75a2ad779e0ca8bebc9f42e6b747d3
Yurim Kim <yurim830@gmail.com>
2025-02-04 04:06:22 +0900

    [Feat] ScreenUtils, homeModalSize 정의 (#17)

---
+++ Staccato-iOS/Staccato-iOS/Util/ScreenUtils.swift
@@ -0,0 +1,25 @@
+//
+//  ScreenUtils.swift
+//  Staccato-iOS
+//
+//  Created by 김유림 on 2/4/25.
+//
+
+import SwiftUI
+
+struct ScreenUtils {
+    
+    // MARK: 디바이스 width
+    
+    static var width: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    
+    // MARK: 디바이스 height
+    
+    static var height: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
+}

0180e13ed6ef21cc856605c7acd4fbefde95ca1f
seunggyun-jeong <jskyun98@gmail.com>
2025-02-14 12:49:26 +0900

    Custom Navigation Bar Title이 가운데로 올 수 있도록 수정

---
+++ Staccato-iOS/Staccato-iOS/Presentation/View/MyPage/MyPageView.swift
@@ -0,0 +1,21 @@
+//
+//  MyPageView.swift
+//  Staccato-iOS
+//
+//  Created by Gyunni on 2/14/25.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        VStack {
+
+        }
+        .staccatoNavigationBar(title: "마이페이지")
+    }
+}
+
+#Preview {
+    MyPageView()
+}

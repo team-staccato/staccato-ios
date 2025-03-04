56b20b77d937821451a3d6d6c41857d85c55d0de
kang-jae-heok <kjhabc8707@naver.com>
2025-03-04 14:55:23 +1100

    [Feat]: ErrorHandler 추가

--- Staccato-iOS/Staccato-iOS/Util/Network/NetworkError.swift
+++ Staccato-iOS/Staccato-iOS/Util/Network/NetworkError.swift
@@ -6,10 +6,37 @@
 //
 
 import Foundation
+import Network
 
 enum NetworkError: Error {
     case invalidURL
-    case requestFailed
-    case decodingFailed
-    case unknown
+    case badRequest
+    case unauthorized
+    case forbidden
+    case notFound
+    case payloadTooLarge
+    case serverError
+    case unknownError
+}
+
+final class ErrorHandler {
+    static func handleError(_ response: HTTPURLResponse?) -> NetworkError {
+        guard let statusCode = response?.statusCode else { return .unknownError }
+        switch statusCode {
+        case 400:
+            return NetworkError.badRequest
+        case 401:
+            return NetworkError.unauthorized
+        case 403:
+            return NetworkError.forbidden
+        case 404:
+            return NetworkError.notFound
+        case 413:
+            return NetworkError.payloadTooLarge
+        case 500...599:
+            return NetworkError.serverError
+        default:
+            return NetworkError.unknownError
+        }
+    }
 }

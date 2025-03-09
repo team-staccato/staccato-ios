19ca49b531d2dbb2fb7e8873693e2ce4224afd81
Yurim Kim <yurim830@gmail.com>
2025-03-10 03:16:57 +0900

    [Add, Feat/#30] STService 생성

---
+++ Staccato-iOS/Staccato-iOS/Util/Network/STService.swift
@@ -0,0 +1,21 @@
+//
+//  StaccatoService.swift
+//  Staccato-iOS
+//
+//  Created by 김유림 on 3/9/25.
+//
+
+import Foundation
+
+class STService {
+    
+    static let shared = STService()
+    
+    init() { }
+    
+    
+    // MARK: - Services
+    
+    lazy var categoryServie = CategoryService()
+    
+}

bc9e27154bcbdb70bfd91687b1b3ea0c3f78c7a5
Yurim Kim <yurim830@gmail.com>
2025-03-10 17:39:45 +0900

    [Feat/#30] basicHeader, headerWithToken 추가

--- Staccato-iOS/Staccato-iOS/Util/Network/HeaderType.swift
+++ Staccato-iOS/Staccato-iOS/Util/Network/HeaderType.swift
@@ -11,11 +11,21 @@ enum HeaderType {
     
     static let noHeader: [String:String] = [:]
     
-    static func token() -> [String: String] {
+    static let basicHeader: [String:String] = ["Content-Type": "application/json"]
+    
+    static func tokenOnly() -> [String:String] {
         if let token = AuthTokenManager.shared.getToken() {
             return ["Authorization" : token]
         } else {
             return noHeader
+        }
+    }
+    
+    static func headerWithToken() -> [String:String] {
+        if let token = AuthTokenManager.shared.getToken() {
+            return ["Content-Type" : "application/json", "Authorization" : token]
+        } else {
+            return basicHeader
         }
     }
     

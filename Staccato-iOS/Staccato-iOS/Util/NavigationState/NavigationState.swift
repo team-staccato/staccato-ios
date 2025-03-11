388d92be1992d39867a32e4f7591fa8a3ed1ea37
Yurim Kim <yurim830@gmail.com>
2025-03-12 04:06:36 +0900

    [Feat/#44] StaccatoDetail에 ID 전달을 위해 NavigationState 수정

--- Staccato-iOS/Staccato-iOS/Util/NavigationState/NavigationState.swift
+++ Staccato-iOS/Staccato-iOS/Util/NavigationState/NavigationState.swift
@@ -10,8 +10,8 @@ import Observation
 
 // MARK: - Destinations
 
-enum CategoryNavigationDestination {
-    case staccatoDetail
+enum CategoryNavigationDestination: Hashable {
+    case staccatoDetail(_ staccatoID: Int64)
     case staccatoAdd
     case categoryDetail
     case categoryAdd
@@ -23,8 +23,20 @@ enum CategoryNavigationDestination {
 @Observable
 class CategoryNavigationState {
     var path = NavigationPath()
+    var lastDestination: CategoryNavigationDestination?
     
     func navigate(to destination: CategoryNavigationDestination) {
-        path.append(destination)
+        // NOTE: 지도 마커 클릭하여 스타카토 -> 스타카토로 가는 경우, 스타카토 경로를 누적하지 않음
+        if case .staccatoDetail = destination,
+           case .staccatoDetail = lastDestination {
+            if path.count > 0 {
+                path.removeLast()
+            }
+            path.append(destination)
+            lastDestination = destination
+        } else {
+            path.append(destination)
+            lastDestination = destination
+        }
     }
 }

dfa82a28bbd5cb8875fc9bb719237be6750c0570
Yurim Kim <yurim830@gmail.com>
2025-03-21 00:50:18 +0900

    [Chore/#39] 마커 클릭 시 fetchStaccatoDetail

--- Staccato-iOS/Staccato-iOS/Presentation/View/Home/GMSMapViewRepresentable.swift
+++ Staccato-iOS/Staccato-iOS/Presentation/View/Home/GMSMapViewRepresentable.swift
@@ -81,7 +81,7 @@ extension GMSMapViewRepresentable.Coordinator: GMSMapViewDelegate {
     
     func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
         if let userdata = marker.userData as? StaccatoCoordinateModel {
-            print("tappedStaccato: \(userdata)")
+            viewModel.fetchStaccatoDetail(userdata.staccatoId)
             viewModel.modalNavigationState.navigate(to: .staccatoDetail(userdata.staccatoId))
         } else {
             print("⚠️ No StaccatoData found for this marker.")

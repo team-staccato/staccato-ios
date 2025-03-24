c613bac3261602115c5da66bf42c28fa371e78b6
Yurim Kim <yurim830@gmail.com>
2025-03-24 16:05:41 +0900

    [Add/#52] StaccatoFeelingRequestDTO 구현

---
+++ Staccato-iOS/Staccato-iOS/Service/Staccato/DTO/PostStaccatoFeelingRequest.swift
@@ -0,0 +1,14 @@
+//
+//  PostStaccatoFeelingRequest.swift
+//  Staccato-iOS
+//
+//  Created by 김유림 on 3/24/25.
+//
+
+import Foundation
+
+struct PostStaccatoFeelingRequest: Encodable {
+    
+    let feeling: String
+    
+}

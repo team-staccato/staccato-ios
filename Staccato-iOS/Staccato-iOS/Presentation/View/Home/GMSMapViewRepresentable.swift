88dc6417a682c6bdd392ecc0469f8e1e6fa66eef
Yurim Kim <yurim830@gmail.com>
2025-03-25 14:31:53 +0900

    [Chore/#39] MainActor 방식 변경 (MainActor.run -> @MainActor)

--- Staccato-iOS/Staccato-iOS/Presentation/View/Home/GMSMapViewRepresentable.swift
+++ Staccato-iOS/Staccato-iOS/Presentation/View/Home/GMSMapViewRepresentable.swift
@@ -81,7 +81,9 @@ extension GMSMapViewRepresentable.Coordinator: GMSMapViewDelegate {
     
     func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
         if let userdata = marker.userData as? StaccatoCoordinateModel {
-            viewModel.fetchStaccatoDetail(userdata.staccatoId)
+            Task {
+                await viewModel.fetchStaccatoDetail(userdata.staccatoId)
+            }
             viewModel.modalNavigationState.navigate(to: .staccatoDetail(userdata.staccatoId))
         } else {
             print("⚠️ No StaccatoData found for this marker.")

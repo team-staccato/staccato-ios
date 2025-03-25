c3f95f5bef057c4c1c43c99d8ac56ca49d68f33c
Yurim Kim <yurim830@gmail.com>
2025-03-25 17:33:02 +0900

    [Feat/#53] PostComment Endpoint 구현

--- Staccato-iOS/Staccato-iOS/Service/Comment/CommentEndpoint.swift
+++ Staccato-iOS/Staccato-iOS/Service/Comment/CommentEndpoint.swift
@@ -10,6 +10,7 @@ import Alamofire
 enum CommentEndpoint {
     
     case getComments(_ staccatoId: Int64)
+    case postComment(_ requestBody: PostCommentRequest)
     
 }
 
@@ -25,24 +26,27 @@ extension CommentEndpoint: APIEndpoint {
     var method: HTTPMethod {
         switch self {
         case .getComments: return .get
+        case .postComment: return .post
         }
     }
     
     var encoding: any Alamofire.ParameterEncoding {
         switch self {
         case .getComments: return URLEncoding.queryString
+        case .postComment: return JSONEncoding.default
         }
     }
     
     var parameters: [String : Any]? {
         switch self {
         case .getComments(let staccatoId): return ["staccatoId" : staccatoId]
+        case .postComment(let requestBody): return requestBody.encode()
         }
     }
     
     var headers: [String : String]? {
         switch self {
-        case .getComments: return HeaderType.tokenOnly()
+        default: return HeaderType.tokenOnly()
         }
     }
     

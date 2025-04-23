88373d372eb0f92d5db4df88de8d9fabc2d1b4ba
Yurim Kim <yurim830@gmail.com>
2025-04-24 03:21:02 +0900

    [Add/#79] StaccatoPlaceModel

---
+++ Staccato-iOS/Staccato-iOS/Model/Staccato/StaccatoPlaceModel.swift
@@ -0,0 +1,19 @@
+//
+//  PlaceModel.swift
+//  Staccato-iOS
+//
+//  Created by 김유림 on 4/24/25.
+//
+
+import Foundation
+import CoreLocation
+
+struct StaccatoPlaceModel {
+
+    let name: String
+
+    let address: String
+
+    let coordinate: CLLocationCoordinate2D
+
+}

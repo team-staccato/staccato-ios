8370980b5f7466d1a955f44c4df9cb80af08d99b
k2645 <k2645@naver.com>
2025-05-24 01:53:04 +0900

    [Chore/#94] KFImage extension 파일 분리

---
+++ Staccato-iOS/Staccato-iOS/Util/Extension/Ex+KFImage.swift
@@ -0,0 +1,23 @@
+//
+//  Ex+KFImage.swift
+//  Staccato-iOS
+//
+//  Created by 김영현 on 5/24/25.
+//
+
+import Kingfisher
+
+extension KFImage {
+    func fillPersonImage(width: CGFloat, height: CGFloat) -> some View {
+        self.placeholder {
+            Image(.personCircleFill)
+                .resizable()
+                .foregroundStyle(.gray2)
+                .frame(width: width, height: height)
+        }
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .frame(width: width, height: height)
+        .clipShape(Circle())
+    }
+}

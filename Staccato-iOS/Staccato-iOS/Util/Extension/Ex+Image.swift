d6dc790eb4104adf3381123016d81792ba52ff15
k2645 <k2645@naver.com>
2025-05-24 01:48:53 +0900

    [Feat/#94] View, KFImage extension 유틸 생성
    overlay 분기처리 코드 유틸
    Circle 프로필 이미지 유틸

--- Staccato-iOS/Staccato-iOS/Util/Extension/Ex+Image.swift
+++ Staccato-iOS/Staccato-iOS/Util/Extension/Ex+Image.swift
@@ -6,9 +6,25 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 extension Image {
     init(_ icon: StaccatoIcon) {
         self.init(systemName: icon.rawValue)
+    }
+}
+
+extension KFImage {
+    func fillPersonImage(width: CGFloat, height: CGFloat) -> some View {
+        self.placeholder {
+            Image(.personCircleFill)
+                .resizable()
+                .foregroundStyle(.gray2)
+                .frame(width: width, height: height)
+        }
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .frame(width: width, height: height)
+        .clipShape(Circle())
     }
 }

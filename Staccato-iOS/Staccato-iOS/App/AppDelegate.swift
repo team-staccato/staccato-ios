ceff25961d713c97549c754d0f5d8e1ad5ee0011
k2645 <k2645@naver.com>
2025-06-30 18:09:45 +0900

    [Feat/#153] 알림 권한 요청 및 확인 로직 수정
    - 앱 최초 실행시 알림 권한 요청
    - 최초 실행이 아닌 경우 알림 권한 확인

--- Staccato-iOS/Staccato-iOS/App/AppDelegate.swift
+++ Staccato-iOS/Staccato-iOS/App/AppDelegate.swift
@@ -30,29 +30,7 @@ final class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate {
         // 알림 센터 델리게이트 설정
         UNUserNotificationCenter.current().delegate = self
         
-        // 권한 요청 및 APNs 등록
-        requestNotificationPermission()
-        
         return true
-    }
-    
-    private func requestNotificationPermission() {
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, error in
-            if let error {
-                print("알림 권한 요청 에러: \(error)")
-                return
-            }
-            
-            Task { @MainActor in
-                if granted {
-                    print("✅ 권한 허용됨 - APNs 등록 시작")
-                    UIApplication.shared.registerForRemoteNotifications()
-                } else {
-                    print("❌ 권한 거부됨")
-                }
-            }
-        }
     }
     
     func application(
@@ -60,31 +38,17 @@ final class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate {
         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
     ) {
         Messaging.messaging().apnsToken = deviceToken
-        registerFCMToken()
     }
     
-    func application(
-        _ application: UIApplication,
-        didFailToRegisterForRemoteNotificationsWithError error: Error
-    ) {
-        print("APNs 토큰 등록 실패: \(error.localizedDescription)")
-    }
-    
-    private func registerFCMToken() {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         Messaging.messaging().token { token, error in
             if let error {
                 print("FCM 토큰 가져오기 실패: \(error)")
             } else if let token {
                 let deviceID = UIDevice.current.identifierForVendor!.uuidString
-                print("FCM 토큰: \(token)")
                 Task { try await NotificationService.postNotificationToken(token, deviceID) }
             }
         }
-    }
-    
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        let deviceID = UIDevice.current.identifierForVendor!.uuidString
-        print("FCM 토큰: \(String(describing: fcmToken)), deviceId: \(deviceID)")
     }
 }
 
